c31e792509200125abcc9611a148f8bff5e3aff3
Thon9 <Thon@ycp.edu>
2014-09-25 16:24:21 -0700

    test commit

--- CS481/src/edu/ycp/cs481/srdesign/HashTag.java
+++ CS481/src/edu/ycp/cs481/srdesign/HashTag.java
@@ -8,6 +8,8 @@ public class HashTag implements Serializable {
 	
 	// Each hash tag will have an ID to be associated with, user accounts will store the IDs of each hash tag they are
 	// following, and the ID will be used to display what the hash tag actually is
+	
+	//
 	private int ID;
 	
 }
