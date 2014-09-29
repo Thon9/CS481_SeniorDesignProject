445acacdef4635a7649f64d28a5d67463bc53c62
cdavis17@ycp.edu <cdavis17@ycp.edu>
2014-09-29 11:59:17 -0400

    Updated HashTag model class

--- CS481/src/edu/ycp/cs481/srdesign/HashTag.java
+++ CS481/src/edu/ycp/cs481/srdesign/HashTag.java
@@ -1,6 +1,7 @@
 package edu.ycp.cs481.srdesign;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class HashTag implements Serializable {
 
@@ -9,5 +10,7 @@ public class HashTag implements Serializable {
 	// Each hash tag will have an ID to be associated with, user accounts will store the IDs of each hash tag they are
 	// following, and the ID will be used to display what the hash tag actually is
 	private int ID;
+	// ArrayList which stores which users are following the individual hashtags.
+	private ArrayList<User> users = new ArrayList<User>();
 	
 }
