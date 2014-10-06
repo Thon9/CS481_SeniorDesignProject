7d89c3eeadb43c2d6eeb9572761367a11e559988
aoduyebo <aoduyebo@ycp.edu>
2014-10-06 12:54:38 -0400

    update

---
+++ CS481/src/edu/ycp/cs481/srdesign/HashTagTest.java
@@ -0,0 +1,14 @@
+package edu.ycp.cs481.srdesign;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HashTagTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ CS481/src/edu/ycp/cs481/srdesign/PhotoTest.java
@@ -0,0 +1,14 @@
+package edu.ycp.cs481.srdesign;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PhotoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
