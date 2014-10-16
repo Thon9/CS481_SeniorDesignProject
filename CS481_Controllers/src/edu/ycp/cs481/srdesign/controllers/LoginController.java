bbf10bb7ae40cc19bee4a6f7081ed6df266386fe
aoduyebo <aoduyebo@ycp.edu>
2014-10-16 14:30:43 -0400

    nothing

--- CS481_Controllers/src/edu/ycp/cs481/srdesign/controllers/LoginController.java
+++ CS481_Controllers/src/edu/ycp/cs481/srdesign/controllers/LoginController.java
@@ -2,6 +2,7 @@ package edu.ycp.cs481.srdesign.controllers;
 
 import edu.ycp.cs481.srdesign.User;
 import edu.ycp.cs481.srdesign.persist.DatabaseProvider;
+import edu.ycp.cs481.srdesign.persist.IDatabase;
 
 public class LoginController {
 	public User login(String username, String password) {
