5d5b6d30151c9e56aa067ed5d9f4c08655ab3eff
Thon9 <Thon@ycp.edu>
2014-10-27 09:43:41 -0700

    created addHashTag servlet

---
+++ CS481_Web/edu/ycp/cs481/servlets/AddHashTag.java
@@ -0,0 +1,49 @@
+package edu.ycp.cs481.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+// import org.apache.catalina.connector.Request;
+
+
+
+import edu.ycp.cs481.srdesign.HashTag;
+import edu.ycp.cs481.srdesign.controllers.AddHashtagController;
+
+/**
+ * Servlet implementation class 
+ */
+@WebServlet("/AddHashTag")
+public class AddHashTag extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setStatus(HttpServletResponse.SC_OK);
+		request.getRequestDispatcher("/main.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HashTag newHashTag = new HashTag();
+		//TODO: get the data of the hashTag
+
+		//if valid hashtag
+		if (newHashTag != null){
+			AddHashtagController controller = new AddHashtagController();
+			//add hashtag to database
+			controller.addHashtag(newHashTag);
+		}
+		//if invalid hashtag e.g. empty hashTag
+		else {
+			request.setAttribute("result", "enter a proper hashTag");
+			this.doGet(request, response);
+		}
+	}
+}
+
--- CS481_Web/edu/ycp/cs481/servlets/Login.java
+++ CS481_Web/edu/ycp/cs481/servlets/Login.java
@@ -24,7 +24,7 @@ import edu.ycp.cs481.srdesign.controllers.LoginController;
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private User user;
-	
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setStatus(HttpServletResponse.SC_OK);
@@ -33,54 +33,51 @@ public class Login extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		//get username and password
 		String userName = request.getParameter("userName");
 		String password = request.getParameter("password");
-		
-		
-		
+
+
+
 		//if username and password is NOT null or empty
 		if (userName != null && password != null && !userName.isEmpty() && !password.isEmpty()){
-		
+
 			//check if user exist
 			System.out.println(userName + " "+ password);
 			//controller should check for the user in the database
-			
+
 			LoginController controller = new LoginController();
-	
+
 			user = controller.login(userName, password);
-			
-				
-				
-				
-				//if user is null 
-				if (controller.login(userName, password) == null){	
-					System.out.println("Incorrect username/password");
-					request.setAttribute("result", "incorrect user/password");
-					this.doGet(request, response);
-				}
-				else {
-					System.out.println("Success");
-					
-					System.out.println(controller.login(userName, password).getUserName());
-	
-					//user exist
-					HttpSession session = request.getSession();
-					session.setAttribute("userName", controller.login(userName, password).getUserName());
-				
-					//to main
-					//request.setAttribute("UserName", "");
-					response.sendRedirect(request.getContextPath()+"/main.jsp");
-					
-					//this.doGet(request, response);
-				}
+
+			//if user is null 
+			if (controller.login(userName, password) == null){	
+				System.out.println("Incorrect username/password");
+				request.setAttribute("result", "incorrect user/password");
+				this.doGet(request, response);
+			}
+			else {
+				System.out.println("Success");
+
+				System.out.println(controller.login(userName, password).getUserName());
+
+				//user exist
+				HttpSession session = request.getSession();
+				session.setAttribute("userName", controller.login(userName, password).getUserName());
+
+				//to main
+				//request.setAttribute("UserName", "");
+				response.sendRedirect(request.getContextPath()+"/main.jsp");
+
+				//this.doGet(request, response);
+			}
 		}
 		else {
 			request.setAttribute("result", "user and/or password fields are empty");
 			this.doGet(request, response);
 		}
-		
+
 	}
 }
 
