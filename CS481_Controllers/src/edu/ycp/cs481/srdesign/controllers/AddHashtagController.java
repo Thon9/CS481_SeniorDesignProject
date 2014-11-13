c1dc7904f180da937ae41cc0218cba0c993e30b9
aoduyebo <aoduyebo@ycp.edu>
2014-11-13 17:14:07 -0500

    moved upload photo UI to photo.jsp from main.jsp

--- CS481_Controllers/src/edu/ycp/cs481/srdesign/controllers/AddHashtagController.java
+++ CS481_Controllers/src/edu/ycp/cs481/srdesign/controllers/AddHashtagController.java
@@ -1,5 +1,7 @@
 package edu.ycp.cs481.srdesign.controllers;
 
+import java.sql.SQLException;
+
 import edu.ycp.cs481.srdesign.HashTag;
 import edu.ycp.cs481.srdesign.persist.DatabaseProvider;
 import edu.ycp.cs481.srdesign.persist.IDatabase;
@@ -7,6 +9,11 @@ import edu.ycp.cs481.srdesign.persist.IDatabase;
 public class AddHashtagController {
 	public void addHashtag(HashTag hashtag) {
 		IDatabase db = DatabaseProvider.getInstance();
-		db.addHashtag(hashtag);
+		try {
+			db.addHashtag(hashtag);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- CS481_Web/edu/ycp/cs481/servlets/AddPhoto.java
+++ CS481_Web/edu/ycp/cs481/servlets/AddPhoto.java
@@ -26,7 +26,7 @@ import edu.ycp.cs481.srdesign.controllers.GetAllPhotosController;
 /**
  * Servlet implementation class
  */
-@WebServlet("/main")
+@WebServlet("/AddPhoto")
 @MultipartConfig
 public class AddPhoto extends HttpServlet {
 	private static final long serialVersionUID = 1L;
