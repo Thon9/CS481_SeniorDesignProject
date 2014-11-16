fc2d68b1517dae46bec9fe914741b00f3233e649
aoduyebo <aoduyebo@ycp.edu>
2014-11-16 17:49:15 -0500

    added db calls for PHOTOHASHTAG table

--- CS481Persistence/src/edu/ycp/cs481/srdesign/SQLDatabase.java
+++ CS481Persistence/src/edu/ycp/cs481/srdesign/SQLDatabase.java
@@ -321,9 +321,43 @@ public ArrayList<Photo> getUserFollowingPhotos(int uID, int hashtagID) {
 	return null;
 }
 
+
+/**
+	 * Might need to be fixed
+	 *******************************************************************************************************************************************************************************************************************************************/
+	
 @Override
-public void addRelaHTP(int hashtagID, int photoID) {
-	// TODO Auto-generated method stub
+public boolean addRelaHTP(final int hashtagID, final int photoID) {
+	try {
+		return executeTransaction(new Transaction<Boolean>() {
+			@Override
+			public Boolean execute(Connection conn) throws SQLException {
+				PreparedStatement preparedStatement = null;
+				try{
+					// Prepare statement
+					preparedStatement = conn.prepareStatement("INSERT INTO PHOTOHASHTAG (PHOTOID, HASHTAGID) VALUES (?, ?)");
+					
+					preparedStatement.setLong(1, photoID);
+					preparedStatement.setLong(2, hashtagID);
+					
+					// Execute Update
+					preparedStatement.executeUpdate();
+					
+				} finally {
+					DBUtil.closeQuietly(preparedStatement);
+					DBUtil.closeQuietly(conn);
+				}
+				return true;
+			}
+
+		});
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		return false;
+	}
+	
+	
 	
 }
 
